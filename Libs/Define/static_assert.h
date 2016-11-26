e46b346f29f29db7327ffcd63fc1a42f18a52531
cuibixiong <cuibixiong@gmail.com>
2016-11-26 10:09:36 +0800

    add missing file

--- Libs/Define/static_assert.h
+++ Libs/Define/static_assert.h
@@ -1,2 +1,11 @@
 #define STATIC_ZERO_ASSERT(condition)(sizeof(struct { int:-!(condition); }))
 #define STATIC_NULL_ASSERT(condition)((void *)STATIC_ZERO_ASSERT(condition)) 
+
+/////////////////////////////////////////
+#define JOIN( X , Y ) JOIN_AGIN( X, Y )
+#define JOIN_AGIN(X,Y) X##Y
+ 
+#define STATIC_ASSERT(e) \
+          typedef char JOIN(assert_failed_at_line,__LINE___) [(e)?1:-1]
+
+/////////////////////////////////////////
