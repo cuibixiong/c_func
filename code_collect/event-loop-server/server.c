3d2ea3f78ec25054d344c956d33a8fd783c14313
cuibixiong <cuibixiong@gmail.com>
2018-06-11 09:35:37 +0800

    Update server.c

--- code_collect/event-loop-server/server.c
+++ code_collect/event-loop-server/server.c
@@ -31,3 +31,23 @@ main (int argc, char *argv[])
 	}
 }
 
+static int
+process_serial_event (void)
+{
+	return 0;
+}
+
+int
+handle_serial_event (int err, remote_client_data client_data)
+{
+  /* Really handle it.  */
+  if (process_serial_event () < 0)
+    return -1;
+
+  /* Be sure to not change the selected thread behind GDB's back.
+     Important in the non-stop mode asynchronous protocol.  */
+  //set_desired_thread (1);
+
+  return 0;
+}
+
