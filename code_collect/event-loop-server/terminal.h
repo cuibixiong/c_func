e8b65a0914f5caedd840cb2f4eb2a0cefcf69ee1
cuibixiong <cuibixiong@gmail.com>
2018-06-11 09:34:54 +0800

    Update terminal.h

--- code_collect/event-loop-server/terminal.h
+++ code_collect/event-loop-server/terminal.h
@@ -1,23 +1,5 @@
-/* Terminal interface definitions for the GDB remote server.
-   Copyright (C) 2002-2013 Free Software Foundation, Inc.
-
-   This file is part of GDB.
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
-
-#if !defined (TERMINAL_H)
-#define TERMINAL_H 1
+#if !defined (__TERMINAL_H__)
+#define __TERMINAL_H__ 1
 
 /* Autoconf will have defined HAVE_TERMIOS_H, HAVE_TERMIO_H,
    and HAVE_SGTTY_H for us as appropriate.  */
